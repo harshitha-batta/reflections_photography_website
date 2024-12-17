d2b135af2565f72e3da332282f15d7ce9f4bfeb2
Glitch (glitch-hello-node) <none>
2024-12-17 15:51:24 +0000

    🎨🚨 Checkpoint ./views/newPassword.ejs:70047169/3350 ./public/reset-password.css:70047169/3344 ./views/resetPassword.ejs:70047169/2217 ./public/likesHandler.js:69912031/893

--- public/likesHandler.js
+++ public/likesHandler.js
@@ -38,29 +38,3 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 });
 
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   const likeBox = document.querySelector(".like-box");
-
-//   if (likeBox) {
-//     likeBox.addEventListener("click", async () => {
-//       const photoId = likeBox.getAttribute("data-photo-id");
-//       const likeCount = document.getElementById("like-count");
-
-//       try {
-//         const response = await fetch(`/like/${photoId}`, { method: "POST" });
-
-//         if (response.ok) {
-//           const result = await response.json(); 
-
-//           likeBox.classList.toggle("liked", result.liked);
-//           likeCount.textContent = result.likesCount;
-//         } else {
-//           alert("Join Reflections to like this photo!");
-//         }
-//       } catch (err) {
-//         console.error("Uh-oh! An unexpected error occurred. Please check your connection and try again. 🌐", err);
-//       }
-//     });
-//   }
-// });
